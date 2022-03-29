da6a6d7facae6a600cc43a46be484022bd3e3946
Aimarest <amarcosestevez@gmail.com>
2022-03-29 09:24:30 +0200

    first function

--- js/main.js
+++ js/main.js
@@ -4,9 +4,18 @@
 
 const button = document.querySelector('.js-button');
 const select = document.querySelector('.js-select');
+const inputNumer = document.querySelector('.js-inputNumber');
+const result = document.querySelector('.js-result');
 
 
 
+//Declarar funciones y eventos
+
+function handleClick (event){
+    
+    event.preventDefault();
+    console.log('holiiiisss');
+}
 
 
 
@@ -16,3 +25,5 @@ const select = document.querySelector('.js-select');
 function getRandomNumber(max) { 
     return Math.ceil(Math.random() * max); 
   } 
+
+  button.addEventListener('click',handleClick);
