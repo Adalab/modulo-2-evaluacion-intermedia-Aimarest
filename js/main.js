4229f98b6ae0a64003de9021e1d13cf8255dc7f4
Aimarest <amarcosestevez@gmail.com>
2022-03-29 10:42:36 +0200

    money

--- js/main.js
+++ js/main.js
@@ -1,37 +1,38 @@
-'use strict';
+"use strict";
 
 // Crear las constantes de los elementos que vamos a utilizar
 
-const button = document.querySelector('.js-button');
-const select = document.querySelector('.js-select');
-const inputNumer = document.querySelector('.js-inputNumber');
-const result = document.querySelector('.js-result');
-const goPlay = document.querySelector('.js-goPlay')
-
+const button = document.querySelector(".js-button");
+const select = document.querySelector(".js-select");
+const inputNumer = document.querySelector(".js-inputNumber");
+const result = document.querySelector(".js-result");
+const goPlay = document.querySelector(".js-goPlay");
+const money = document.querySelector(".js-money");
 
 //Declarar funciones y eventos
-function getRandomNumber(max) { 
-    return Math.ceil(Math.random() * max); 
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
 }
-function handleClick (event){
-    event.preventDefault();
+function handleClick(event) {
+  event.preventDefault();
 
-   const number = getRandomNumber(6);
+  const number = getRandomNumber(6);
   const optionSelected = select.value;
-  result.innerHTML = number;
-  console.log(number);
+  const apostado = inputNumer.value;
+  result.innerHTML = `Resultado ${number}`;
   if (number === parseInt(optionSelected)) {
-      goPlay.innerHTML = 'Has ganado el doble de lo apostado! :)'
+    goPlay.innerHTML = "Has ganado el doble de lo apostado! :)";
+    addMoney(apostado);
   } else {
-      goPlay.innerHTML = 'Has perdido todo lo apostado! :('
+    goPlay.innerHTML = "Has perdido todo lo apostado! :(";
+    restMoney(apostado);
   }
-};
+}
+button.addEventListener("click", handleClick);
 
-
-
-
-
-
-
-
-  button.addEventListener('click',handleClick);
+function addMoney(apostado) {
+  money.innerHTML = parseInt(money.innerHTML) + 2 * apostado;
+}
+function restMoney(apostado) {
+  money.innerHTML = parseInt(money.innerHTML) - apostado;
+}
