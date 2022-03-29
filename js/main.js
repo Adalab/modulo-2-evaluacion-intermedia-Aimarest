780f6f819ea72795c97f22245abfeabfb9425416
Aimarest <amarcosestevez@gmail.com>
2022-03-29 09:15:14 +0200

    option disabled

---
+++ js/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Crear las constantes de los elementos que vamos a utilizar
+
+const button = document.querySelector('.js-button');
+const select = document.querySelector('.js-select');
+
+
+
+
+
+
+
+
+
+function getRandomNumber(max) { 
+    return Math.ceil(Math.random() * max); 
+  } 
